346811b0e9fda3f89ada10527bdab9b39f3687d4
innovationchef <ankit.lohani.iitkgp@gmail.com>
2018-05-28 22:08:20 +0530

    Scrapy will select only relevant settings from config

--- bioschemas_scraper/run.py
+++ bioschemas_scraper/run.py
@@ -3,19 +3,17 @@ from scrapy import cmdline
 from six.moves.configparser import ConfigParser
 
 config_file = "../config/settings.ini"
+allowed_settings_scrapy = ['MongoDBServer', 'Logging', 'Concurrency Settings']
 parser = ConfigParser()
 parser.read(config_file)
 settings = str()
 for section_name in parser.sections():
-    # print('Section:', section_name)
-    # print('  Options:', parser.options(section_name))
-    # for name, value in parser.items(section_name):
-    #     print('  {} = {}'.format(name, value))
-    setting = str()
-    for name, value in  parser.items(section_name):
-        setting = setting + str('--set=') + str(name) + '=' + str(value) + str(' ')
-    
-    settings = settings + setting
+    if section_name in allowed_settings_scrapy:
+        setting = str()
+        for name, value in  parser.items(section_name):
+            setting = setting + str('--set=') + str(name) + '=' + str(value) + str(' ')
+        
+        settings = settings + setting
 
 execute = "scrapy crawl " + settings +  "sitemap"
 cmdline.execute(execute.split())
